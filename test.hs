17be34d489e4ca6cc127ebda8d1b46a1cfe29703
tomabou <kazukazu496@yahoo.co.jp>
2017-04-04 17:06:11 +0900

    solve ABC

--- test.hs
+++ test.hs
@@ -1,2 +1,17 @@
+import Data.List
+import Data.Function
+import Control.Monad
 main = do
-    putStrLn "Hello World"
+    (n,m) <- inputPair
+    as <- replicateM n inputPair
+    cs <- replicateM m inputPair
+    mapM print $ map (solve cs) as
+
+inputPair :: IO (Int,Int)
+inputPair = (\[x,y] -> (x,y)).(map read).words <$> getLine
+
+manDist :: (Int ,Int) -> (Int,Int) -> Int
+manDist (a,b) (c,d) = abs (a-c) + abs (b-d)
+
+solve :: [(Int,Int)] -> (Int,Int) -> Int
+solve xs a=fst $ minimumBy (compare `on` (manDist a).snd) (zip [1..] xs)
