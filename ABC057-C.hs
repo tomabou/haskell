345b8f93a7df69f80ebc020497a1c3ad77a36f31
HomePC <kazu.8182.9.6@gmail.com>
2018-03-16 00:20:34 +0900

    整理

--- ABC057-C.hs
+++
@@ -1,4 +0,0 @@
-main = do
-    n <- readLn :: IO Integer
-    print.minimum.(map $ length.show.(div n))
-            .(filter $ (==0).(mod n)) $ [1..(ceiling .sqrt. fromIntegral $ n)]
--- Atcoder/abc.hs
+++
@@ -1,6 +0,0 @@
-main = do
-    a <- getLine
-    b <- getLine
-    putStrLn $ func a b
-func xs [] = xs
-func (x:xs) (y:ys) = x:y:func xs ys
